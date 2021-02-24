47a31f97598891d2cc6f3b5fdacfff7b13c289b9
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-02-23 17:39:35 -0700

    updates

--- greet.js
+++ greet.js
@@ -1,1 +1,8 @@
 const getInput = require("./get-input.js");
+
+//use console.log to print out a greeting to user.
+
+// 1. get the user's name and save it in a variable.
+const name = getInput();
+// 2. print out what's in that variable.
+console.log("Hello, " + name + "!");
--- mental-age.js
+++ mental-age.js
@@ -1,1 +1,20 @@
 const getInput = require("./get-input.js");
+// 1. take in our user's age, saving it in a variable.
+const userAge = getInput ()
+
+// 2. check if they want help.
+if (userAge === "--help"){
+    console.log("This function returns your mental age.");
+    console.log("Please enter your age after 'node mental-age.js' and a space");
+}
+
+//3. divide it by 2 and add 3.
+const mentalAge = userAge / 2 + 3;
+const ageNextYr = Number(userAge) + 1;
+//4. print out the result in a sentence.
+// for example, "You are 23 years old."
+console.log("You are " + userAge + " years old.");
+console.log("Next year you will be  " + ageNextYr + " years old.");
+
+//5. print out the user's mental age in a sentence.
+console.log("Your mental age is " + mentalAge + " years old.");
